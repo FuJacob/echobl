788d60b775b1ef9fa3c47b3fd3591786fcea1b9d
Jacob Fu <jjacobfu@gmail.com>
2025-01-02 23:31:35 -0500

    changed some things

--- echobl/settings.py
+++ echobl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-im_g)$l%1gt7om%5i7^&b)qdre_!8gui@r2)=r#f39iv*^zeap'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "True"
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get("ALLOWED HOSTS").split(" ")
 
 # Application definition
 
@@ -83,8 +82,7 @@ DATABASES = {
     }
 }
 
-DATABASES['default'] = dj_database_url.parse("postgresql://echobl_user:czln5ym0c9hz5NGNFBdqG102CwQ9pyDK@dpg-ctrmavq3esus73df2li0-a.ohio-postgres.render.com/echobl")
-
+DATABASES['default'] = dj_database_url.parse(databaseurl)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
